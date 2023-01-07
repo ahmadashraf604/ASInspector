d3bdd56898566a6e454ac9d03194860a2824bbc9
Ahmed Ashraf <ahmadashraf604@gmail.com>
2023-01-07 18:56:03 +0200

    Add ASInspector Framework

---
+++ ASInspector/ASInspector/ASInspector.h
@@ -0,0 +1,18 @@
+//
+//  ASInspector.h
+//  ASInspector
+//
+//  Created by Ahmed Ashraf on 07/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASInspector.
+FOUNDATION_EXPORT double ASInspectorVersionNumber;
+
+//! Project version string for ASInspector.
+FOUNDATION_EXPORT const unsigned char ASInspectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASInspector/PublicHeader.h>
+
+
